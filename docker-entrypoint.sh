e273be681cfada750e4b2dba3b8539848f2156bb
JayD152 <54570374+JayD152@users.noreply.github.com>
2026-03-06 00:34:25 -0500

    modify docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-./node_modules/.bin/prisma db push --skip-generate
+node ./node_modules/prisma/build/index.js db push --skip-generate
 echo "Migrations complete."
 
 exec "$@"
